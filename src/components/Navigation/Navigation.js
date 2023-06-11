72564b652367685460bffa8ae7d2d62372592c9b
mateuszNadolny <mateusznadolny00@gmail.com>
2023-06-11 17:02:39 +0200

    refractor: refractor the component's HTML semantic so that now it renders according to convention

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -9,7 +9,7 @@ import classes from './Navigation.module.css';
 const NavigationBar = () => {
   const pathname = usePathname();
   return (
-    <footer className="fixed bottom-0 w-full flex justify-content-center pb-3 pt-3 bg-white-alpha-90">
+    <footer className="fixed bottom-0 w-full flex justify-content-center pb-3 pt-3 bg-white-alpha-90 shadow-5">
       <ul className="flex p-0 m-0 mb-1 list-none gap-5 ">
         <li>
           <div className="flex flex-column align-items-center fadein transition-duration-300">
